d55354057ca7e51a562a0b15a617491415fc93bc
GASANA Eddy <eddygasana13@gmail.com>
2023-01-15 19:29:02 +0200

    Deployment Commit Updated

--- URSS/settings.py
+++ URSS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5%a8gdzd9%nd@eg1q128^8w23dmid-kic64xytyw*nuzq*&%+6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'blogApp.apps.BlogappConfig',
     'django.contrib.humanize',
     'crispy_forms',
-    'django_bootstrap_icons',
+    
 ]
 
 MIDDLEWARE = [
@@ -127,12 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (
-    # location of your application, should not be public web accessible 
-    './static',
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
